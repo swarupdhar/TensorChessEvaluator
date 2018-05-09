1ea52e40fe5048f89b4d93c636c142542823d4ea
Swarup Dhar <student@Swarup-Dhar.local>
2018-05-09 10:55:19 -0400

    Updated to match the new base implementation

--- models/simple.py
+++ models/simple.py
@@ -3,24 +3,7 @@ import tensorflow as tf
 
 class SimpleModel(BaseModel):
     def __init__(self, sess:tf.Session):
-        super().__init__({
-            'shape' : [65, 3, 1],
-            'sess': sess
-        })
-    
-    def init_weights_and_biases(self):
-        for i, layer_size in enumerate(self.shape):
-            if i == self.num_layers - 1:
-                break
-            next_layer_size = self.shape[i+1]
-            self.weights[i] = tf.Variable(
-                tf.random_normal([layer_size, next_layer_size]), 
-                name=f"weight_{i}"
-            )
-            self.biases[i] = tf.Variable(
-                tf.ones([1, next_layer_size]),
-                name=f"bias_{i}"
-            )
+        super().__init__([65, 3, 1], sess)
     
     def build_model(self) -> tf.Tensor:
         current = self.inputs
