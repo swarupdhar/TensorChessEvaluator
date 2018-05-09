945818c2dca81ecd474e59b45fecefbdf79eee0a
Swarup Dhar <student@Swarup-Dhar.local>
2018-05-09 10:20:14 -0400

    Added the prediction function to the base class.

--- models/base.py
+++ models/base.py
@@ -39,9 +39,14 @@ class BaseModel:
         return tf.train.AdamOptimizer(learning_rate=self.learning_rate).minimize(self.get_loss_function())
 
     def train(self, data_loader:DataLoader):
-        x, y = data_loader.next_batch()
         for i in range(self.epochs):
+            x, y = data_loader.next_batch()
             self.sess.run(self.trainer, feed_dict={
                 self.inputs: x,
                 self.targets: y
             })
+    
+    def predict(self, x:List[List[float]]) -> List[List[float]]:
+        return self.sess.run(self.model, feed_dict={
+            self.inputs: x
+        })
